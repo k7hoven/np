f9474c7f99d5abd3e3497d929cfc37a9789004f1
Koos Zevenhoven <koos.zevenhoven@aalto.fi>
2016-03-27 21:12:46 +0300

    Refactor quick types into separate non-module type

--- np/magic.py
+++ np/magic.py
@@ -33,6 +33,29 @@ np_quick_types = {
     'c64': numpy.complex64 
 }
                   
+
+class NPQuickTypeShortcut(object):
+    def __init__(self, shortcut):
+        self.dtype = np_quick_types[shortcut]
+        self._shortcut_name = 'np.' + shortcut
+        
+    def __getitem__(self, arg):
+        if isinstance(arg, tuple):
+            # Assume the tuple was not created by the user,
+            # i.e. multiple arguments to subscript [arg1, ...].
+            return array(arg, dtype = self.dtype)
+        if isinstance(arg, list):
+            return array((arg,), dtype = self.dtype)
+        if isinstance(arg, slice):
+            rangeargs = (arg.start if arg.start is not None else 0, 
+                         arg.stop, 
+                         arg.step if arg.step is not None else 1)
+            return arange(*rangeargs, dtype = self.dtype)
+        return array((arg,), dtype = self.dtype)
+  
+    def __call__(self, *args, **kwargs):
+        return asanyarray(*args, dtype = self.dtype, **kwargs)
+    
                 
 
 class npmodule(types.ModuleType):
@@ -41,25 +64,8 @@ class npmodule(types.ModuleType):
             self.__name__ = numpy.__name__ # to initialize self.__dict__
             self.__dict__.update(numpy.__dict__)
             
-            self.f = npmodule(dtype = numpy.float_)
-            self.f64 = npmodule(dtype = numpy.float64)        
-            self.f32 = npmodule(dtype = numpy.float32)            
-            
-            self.i = npmodule(dtype = numpy.int_)
-            self.i64 = npmodule(dtype = numpy.int64)
-            self.i32 = npmodule(dtype = numpy.int32)
-            self.i16 = npmodule(dtype = numpy.int16)
-            #self.i8 = npmodule(dtype = numpy.int8)
-            
-            self.ui64 = npmodule(dtype = numpy.uint64)
-            self.ui32 = npmodule(dtype = numpy.uint32)
-            self.ui16 = npmodule(dtype = numpy.uint16)
-            #self.ui8 = npmodule(dtype = numpy.uint8)
-            
-            self.c = npmodule(dtype = numpy.complex_)
-            self.c128 = npmodule(dtype = numpy.complex128)
-            self.c64 = npmodule(dtype = numpy.complex64)
-            
+            for shortcut in np_quick_types:
+                setattr(self, shortcut, NPQuickTypeShortcut(shortcut))
             self.np_quick_types = np_quick_types            
                         
             self.numpy = numpy
