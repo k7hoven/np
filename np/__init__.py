1c2c02f701fcf7b5088215a10714b51177d9ee50
Koos Zevenhoven <koos.zevenhoven@aalto.fi>
2017-09-28 20:14:24 +0300

    Cleanup

--- np/__init__.py
+++ np/__init__.py
@@ -3,7 +3,6 @@
 
 # Needs to do some magic to import numpy and replace it with a monkey-patched
 # version implementing __getitem__ etc. This seems to be close to the cleanest
-# way to do this in Python. This now works best with Python >=3.5.
+# way to do this in Python. This now works best with Python >=3.5
 
-import n
-p.magic
+import np.magic
